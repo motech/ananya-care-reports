49c0221067d1e60b3187494402f764ea431bcbae
Adrian Boguszewski <aboguszewski@soldevelo.com>
2013-07-01 17:58:52 +0200

    CARE #6: Fixed Dao templates errors

---
+++ src/main/java/org/motechproject/carereporting/dao/IndicatorCategoryDao.java
@@ -0,0 +1,6 @@
+package org.motechproject.carereporting.dao;
+
+import org.motechproject.carereporting.domain.IndicatorCategoryEntity;
+
+public interface IndicatorCategoryDao extends GenericDao<IndicatorCategoryEntity> {
+}
---
+++ src/main/java/org/motechproject/carereporting/dao/LevelDao.java
@@ -0,0 +1,6 @@
+package org.motechproject.carereporting.dao;
+
+import org.motechproject.carereporting.domain.LevelEntity;
+
+public interface LevelDao extends GenericDao<LevelEntity> {
+}
