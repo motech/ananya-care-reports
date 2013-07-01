94b87d6f1663140b805fa5c11f0443628ba1b264
Lech Rozanski <lrozanski@soldevelo.com>
2013-07-01 09:34:12 +0200

    CARE #20: Updated database creation sql

--- src/main/resources/db/create_tables.sql
+++ src/main/resources/db/create_tables.sql
@@ -1,293 +1,369 @@
---
--- PostgreSQL database dump
---
+-- Database generated with pgModeler (PostgreSQL Database Modeler).
+-- PostgreSQL version: 9.2
+-- Project Site: pgmodeler.com.br
+-- Model Author: ---
 
--- Dumped from database version 9.1.9
--- Dumped by pg_dump version 9.1.9
--- Started on 2013-06-28 11:06:38 CEST
-
-SET statement_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
 SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- TOC entry 168 (class 3079 OID 11679)
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+-- ddl-end --
 
 
---
--- TOC entry 1942 (class 0 OID 0)
--- Dependencies: 168
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
+-- Database creation must be done outside an multicommand file.
+-- These commands were put in this file only for convenience.
+-- -- object: carereporting | type: DATABASE --
+-- CREATE DATABASE carereporting
+-- ;
+-- -- ddl-end --
+-- 
 
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- TOC entry 161 (class 1259 OID 21166)
--- Dependencies: 5
--- Name: care_indicator; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
---
-
-CREATE TABLE care_indicator (
-    indicator_id integer NOT NULL,
-    creation_date timestamp without time zone,
-    modification_date timestamp without time zone,
-    name character varying(255) NOT NULL
+-- object: public.indicator_type | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.indicator_type(
+	indicator_type_id serial NOT NULL,
+	name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT indicator_type_pk PRIMARY KEY (indicator_type_id),
+	CONSTRAINT indicator_type_name_uk UNIQUE (name)
 );
+-- ddl-end --
 
+-- ddl-end --
 
-ALTER TABLE public.care_indicator OWNER TO postgres;
-
---
--- TOC entry 162 (class 1259 OID 21171)
--- Dependencies: 5
--- Name: care_user; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE care_user (
-    user_id integer NOT NULL,
-    creation_date timestamp without time zone,
-    modification_date timestamp without time zone,
-    password character varying(255),
-    username character varying(255)
+-- object: public.indicator_category | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.indicator_category(
+	indicator_category_id serial NOT NULL,
+	name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT indicator_category_pk PRIMARY KEY (indicator_category_id),
+	CONSTRAINT indicator_category_name_uk UNIQUE (name)
 );
+-- ddl-end --
 
+-- ddl-end --
 
-ALTER TABLE public.care_user OWNER TO postgres;
-
---
--- TOC entry 167 (class 1259 OID 21226)
--- Dependencies: 5
--- Name: hibernate_sequence; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE hibernate_sequence
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.hibernate_sequence OWNER TO postgres;
-
---
--- TOC entry 163 (class 1259 OID 21179)
--- Dependencies: 5
--- Name: permission; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE permission (
-    permission_id integer NOT NULL,
-    creation_date timestamp without time zone,
-    modification_date timestamp without time zone,
-    display_name character varying(255),
-    name character varying(255)
+-- object: public.level | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.level(
+	level_id serial NOT NULL,
+	parent_level_id integer NOT NULL,
+	name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT level_pk PRIMARY KEY (level_id),
+	CONSTRAINT level_parent_level_id_fk FOREIGN KEY (parent_level_id)
+	REFERENCES public.level (level_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT level_name_uk UNIQUE (name)
 );
+-- ddl-end --
 
+-- ddl-end --
 
-ALTER TABLE public.permission OWNER TO postgres;
-
---
--- TOC entry 164 (class 1259 OID 21187)
--- Dependencies: 5
--- Name: role; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE role (
-    role_id integer NOT NULL,
-    creation_date timestamp without time zone,
-    modification_date timestamp without time zone,
-    name character varying(255)
+-- object: public.area | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.area(
+	area_id serial NOT NULL,
+	level_id integer NOT NULL,
+	name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT area_pk PRIMARY KEY (area_id),
+	CONSTRAINT area_level_id_fk FOREIGN KEY (area_id)
+	REFERENCES public.level (level_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT area_name_uk UNIQUE (name)
 );
+-- ddl-end --
 
+-- ddl-end --
 
-ALTER TABLE public.role OWNER TO postgres;
-
---
--- TOC entry 165 (class 1259 OID 21192)
--- Dependencies: 5
--- Name: role_permission; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE role_permission (
-    role_id integer NOT NULL,
-    permission_id integer NOT NULL
+-- object: public.chart_type | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.chart_type(
+	chart_type_id serial NOT NULL,
+	name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT chart_type_pk PRIMARY KEY (chart_type_id),
+	CONSTRAINT chart_type_name_uk UNIQUE (name)
 );
+-- ddl-end --
 
+-- ddl-end --
 
-ALTER TABLE public.role_permission OWNER TO postgres;
-
---
--- TOC entry 166 (class 1259 OID 21197)
--- Dependencies: 5
--- Name: user_role; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE user_role (
-    user_id integer NOT NULL,
-    role_id integer NOT NULL
+-- object: public.report_type | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.report_type(
+	report_type_id integer NOT NULL,
+	chart_type_id integer,
+	name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT report_type_pk PRIMARY KEY (report_type_id),
+	CONSTRAINT report_chart_type_id_fk FOREIGN KEY (chart_type_id)
+	REFERENCES public.chart_type (chart_type_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT report_type_name_uk UNIQUE (name)
 );
+-- ddl-end --
 
+-- ddl-end --
 
-ALTER TABLE public.user_role OWNER TO postgres;
+-- object: public.report | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.report(
+	report_id serial NOT NULL,
+	type_id integer NOT NULL,
+	name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT report_pk PRIMARY KEY (report_id),
+	CONSTRAINT report_type_id_fk FOREIGN KEY (type_id)
+	REFERENCES public.report_type (report_type_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT report_name_uk UNIQUE (name)
+);
+-- ddl-end --
 
---
--- TOC entry 1943 (class 0 OID 0)
--- Dependencies: 167
--- Name: hibernate_sequence; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
+-- ddl-end --
 
-SELECT pg_catalog.setval('hibernate_sequence', 3, true);
+-- object: public.comparison_symbol | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.comparison_symbol(
+	comparison_symbol_id serial NOT NULL,
+	name character varying(10) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT comparison_symbol_pk PRIMARY KEY (comparison_symbol_id),
+	CONSTRAINT comparison_symbol_name_uk UNIQUE (name)
+);
+-- ddl-end --
 
---
--- TOC entry 1909 (class 2606 OID 21170)
--- Dependencies: 161 161 1936
--- Name: care_indicator_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
+-- ddl-end --
 
-ALTER TABLE ONLY care_indicator
-    ADD CONSTRAINT care_indicator_pkey PRIMARY KEY (indicator_id);
+-- object: public.form | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.form(
+	form_id serial NOT NULL,
+	table_name character varying(50) NOT NULL,
+	display_name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT form_pk PRIMARY KEY (form_id),
+	CONSTRAINT form_table_name_uk UNIQUE (table_name),
+	CONSTRAINT form_display_name_uk UNIQUE (display_name)
+);
+-- ddl-end --
 
+-- ddl-end --
 
---
--- TOC entry 1911 (class 2606 OID 21178)
--- Dependencies: 162 162 1936
--- Name: care_user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
+-- object: public.operator_type | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.operator_type(
+	operator_type_id serial NOT NULL,
+	name character varying(10) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT operator_type_pk PRIMARY KEY (operator_type_id),
+	CONSTRAINT operator_type_name_uk UNIQUE (name)
+);
+-- ddl-end --
 
-ALTER TABLE ONLY care_user
-    ADD CONSTRAINT care_user_pkey PRIMARY KEY (user_id);
+-- ddl-end --
 
+-- object: public.complex_condition | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.complex_condition(
+	complex_condition_id serial NOT NULL,
+	operator_type_id integer NOT NULL,
+	form_id integer NOT NULL,
+	comparison_symbol_id integer NOT NULL,
+	field character varying(100) NOT NULL,
+	comparison_value decimal(19,2) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT complex_condition_pk PRIMARY KEY (complex_condition_id),
+	CONSTRAINT complex_condition_operator_type_id FOREIGN KEY (operator_type_id)
+	REFERENCES public.operator_type (operator_type_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT complex_condition_form_id FOREIGN KEY (form_id)
+	REFERENCES public.form (form_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT complex_condition_comparison_symbol_id FOREIGN KEY (comparison_symbol_id)
+	REFERENCES public.comparison_symbol (comparison_symbol_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE
+);
+-- ddl-end --
 
---
--- TOC entry 1915 (class 2606 OID 21205)
--- Dependencies: 163 163 1936
--- Name: name_; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
+-- ddl-end --
 
-ALTER TABLE ONLY permission
-    ADD CONSTRAINT name_ UNIQUE (name);
+-- object: public.care_user | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.care_user(
+	user_id serial NOT NULL,
+	username character varying(100) NOT NULL,
+	password character varying(40) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT user_pk PRIMARY KEY (user_id),
+	CONSTRAINT user_username_uk UNIQUE (username)
+);
+-- ddl-end --
 
+-- ddl-end --
 
---
--- TOC entry 1917 (class 2606 OID 21186)
--- Dependencies: 163 163 1936
--- Name: permission_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
+-- object: public.permission | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.permission(
+	permission_id serial NOT NULL,
+	name character varying(100) NOT NULL,
+	display_name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT permission_pk PRIMARY KEY (permission_id),
+	CONSTRAINT permission_name_uk UNIQUE (name),
+	CONSTRAINT permission_display_name_uk UNIQUE (display_name)
+);
+-- ddl-end --
 
-ALTER TABLE ONLY permission
-    ADD CONSTRAINT permission_pkey PRIMARY KEY (permission_id);
+-- ddl-end --
 
+-- object: public.role | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.role(
+	role_id serial NOT NULL,
+	name character varying(100) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT role_pk PRIMARY KEY (role_id),
+	CONSTRAINT role_name_uk UNIQUE (name)
+);
+-- ddl-end --
 
---
--- TOC entry 1921 (class 2606 OID 21196)
--- Dependencies: 165 165 165 1936
--- Name: role_permission_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
+-- ddl-end --
 
-ALTER TABLE ONLY role_permission
-    ADD CONSTRAINT role_permission_pkey PRIMARY KEY (role_id, permission_id);
+-- object: public.care_user_role | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.care_user_role(
+	user_id integer NOT NULL,
+	role_id integer NOT NULL,
+	CONSTRAINT user_role_user_id_fk FOREIGN KEY (user_id)
+	REFERENCES public.care_user (user_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT user_role_role_id_fk FOREIGN KEY (role_id)
+	REFERENCES public.role (role_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT user_role_uk UNIQUE (user_id,role_id)
+);
+-- ddl-end --
 
+-- ddl-end --
 
---
--- TOC entry 1919 (class 2606 OID 21191)
--- Dependencies: 164 164 1936
--- Name: role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
+-- object: public.role_permission | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.role_permission(
+	role_id integer NOT NULL,
+	permission_id integer NOT NULL,
+	CONSTRAINT role_permission_role_id_fk FOREIGN KEY (role_id)
+	REFERENCES public.role (role_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT role_permission_permission_id_fk FOREIGN KEY (permission_id)
+	REFERENCES public.permission (permission_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT role_permission_uk UNIQUE (role_id,permission_id)
+);
+-- ddl-end --
 
-ALTER TABLE ONLY role
-    ADD CONSTRAINT role_pkey PRIMARY KEY (role_id);
+-- ddl-end --
 
+-- object: public.indicator | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.indicator(
+	indicator_id serial NOT NULL,
+	type_id integer NOT NULL,
+	category_id integer NOT NULL,
+	top_level_id integer NOT NULL,
+	owner_id integer NOT NULL,
+	frequency integer NOT NULL,
+	name character varying NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT indicator_pk PRIMARY KEY (indicator_id),
+	CONSTRAINT indicator_type_id_fk FOREIGN KEY (type_id)
+	REFERENCES public.indicator_type (indicator_type_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT indicator_category_id_fk FOREIGN KEY (category_id)
+	REFERENCES public.indicator_category (indicator_category_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT indicator_top_level_id_fk FOREIGN KEY (top_level_id)
+	REFERENCES public.level (level_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT indicator_owner_id_fk FOREIGN KEY (owner_id)
+	REFERENCES public.care_user (user_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT indicator_name_uk UNIQUE (name)
+);
+-- ddl-end --
 
---
--- TOC entry 1923 (class 2606 OID 21201)
--- Dependencies: 166 166 166 1936
--- Name: user_role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
+-- ddl-end --
 
-ALTER TABLE ONLY user_role
-    ADD CONSTRAINT user_role_pkey PRIMARY KEY (user_id, role_id);
+-- object: public.dashboard | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.dashboard(
+	dashboard_id serial NOT NULL,
+	owner_id integer NOT NULL,
+	tab_position smallint NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT dashboard_pk PRIMARY KEY (dashboard_id),
+	CONSTRAINT dashboard_user_id_fk FOREIGN KEY (owner_id)
+	REFERENCES public.care_user (user_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT dashboard_tab_position_uk UNIQUE (tab_position)
+);
+-- ddl-end --
 
+-- ddl-end --
 
---
--- TOC entry 1913 (class 2606 OID 21203)
--- Dependencies: 162 162 1936
--- Name: username_; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
+-- object: public.report_dashboard | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.report_dashboard(
+	report_id integer NOT NULL,
+	dashboard_id integer NOT NULL,
+	CONSTRAINT report_dashboard_report_fk FOREIGN KEY (report_id)
+	REFERENCES public.report (report_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT report_dashboard_dashboard_id_fk FOREIGN KEY (dashboard_id)
+	REFERENCES public.dashboard (dashboard_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT report_dashboard_uk UNIQUE (report_id,dashboard_id)
+);
+-- ddl-end --
 
-ALTER TABLE ONLY care_user
-    ADD CONSTRAINT username_ UNIQUE (username);
+-- ddl-end --
 
+-- object: public.indicator_report | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.indicator_report(
+	indicator_id integer NOT NULL,
+	report_id integer NOT NULL,
+	CONSTRAINT indicator_report_indicator_id FOREIGN KEY (indicator_id)
+	REFERENCES public.indicator (indicator_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT indicator_report_report_id_fk FOREIGN KEY (report_id)
+	REFERENCES public.report (report_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT indicator_report_uk UNIQUE (indicator_id,report_id)
+);
+-- ddl-end --
 
---
--- TOC entry 1927 (class 2606 OID 21221)
--- Dependencies: 1910 162 166 1936
--- Name: fk143bf46a4f5e391f; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
+-- ddl-end --
 
-ALTER TABLE ONLY user_role
-    ADD CONSTRAINT fk143bf46a4f5e391f FOREIGN KEY (user_id) REFERENCES care_user(user_id);
+-- object: public.indicator_value | type: TABLE --
+CREATE TABLE IF NOT EXISTS public.indicator_value(
+	indicator_value_id serial NOT NULL,
+	indicator_id integer NOT NULL,
+	area_id integer NOT NULL,
+	condition_id integer NOT NULL,
+	date timestamp NOT NULL,
+	condition_value decimal(19,2) NOT NULL,
+	creation_date timestamp,
+	modification_date timestamp,
+	CONSTRAINT indicator_value_pk PRIMARY KEY (indicator_value_id),
+	CONSTRAINT indicator_value_indicator_id_fk FOREIGN KEY (indicator_id)
+	REFERENCES public.indicator (indicator_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT indicator_value_area_id_fk FOREIGN KEY (area_id)
+	REFERENCES public.area (area_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE,
+	CONSTRAINT indicator_value_condition_id_fk FOREIGN KEY (condition_id)
+	REFERENCES public.complex_condition (complex_condition_id) MATCH FULL
+	ON DELETE NO ACTION ON UPDATE NO ACTION NOT DEFERRABLE
+);
+-- ddl-end --
 
+-- ddl-end --
 
---
--- TOC entry 1926 (class 2606 OID 21216)
--- Dependencies: 164 166 1918 1936
--- Name: fk143bf46aa3be08ff; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY user_role
-    ADD CONSTRAINT fk143bf46aa3be08ff FOREIGN KEY (role_id) REFERENCES role(role_id);
-
-
---
--- TOC entry 1924 (class 2606 OID 21206)
--- Dependencies: 1916 163 165 1936
--- Name: fkbd40d53894070e1f; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY role_permission
-    ADD CONSTRAINT fkbd40d53894070e1f FOREIGN KEY (permission_id) REFERENCES permission(permission_id);
-
-
---
--- TOC entry 1925 (class 2606 OID 21211)
--- Dependencies: 165 1918 164 1936
--- Name: fkbd40d538a3be08ff; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY role_permission
-    ADD CONSTRAINT fkbd40d538a3be08ff FOREIGN KEY (role_id) REFERENCES role(role_id);
-
-
---
--- TOC entry 1941 (class 0 OID 0)
--- Dependencies: 5
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
--- Completed on 2013-06-28 11:06:38 CEST
-
---
--- PostgreSQL database dump complete
---
 
--- src/main/resources/db/insert_initial_data.sql
+++ src/main/resources/db/insert_initial_data.sql
@@ -3,4 +3,4 @@ INSERT INTO care_user (user_id, username, password,  creation_date, modification
 INSERT INTO role (role_id, name) VALUES (1, 'Admin');
 INSERT INTO permission(permission_id, name, display_name) VALUES (1, 'CAN_CREATE_INDICATORS', 'Can create indicators');
 INSERT INTO role_permission (role_id, permission_id) VALUES (1, 1);
-INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO care_user_role (user_id, role_id) VALUES (1, 1);
