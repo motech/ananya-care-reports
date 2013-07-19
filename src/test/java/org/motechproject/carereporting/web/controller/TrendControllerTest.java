3bcdfec7e046f350f6821ed0b460f75bea58f4a0
Pawel Reise <preise@soldevelo.com>
2013-07-19 14:08:38 +0200

    CARE #8: Added test for TrendController

---
+++ src/test/java/org/motechproject/carereporting/web/controller/TrendControllerTest.java
@@ -0,0 +1,60 @@
+package org.motechproject.carereporting.web.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.motechproject.carereporting.domain.UserEntity;
+import org.motechproject.carereporting.service.IndicatorService;
+import org.motechproject.carereporting.service.UserService;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Date;
+
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrendControllerTest {
+
+    private static final String GET_TRENDS_START_DATE = "23-05-2013";
+
+    private static final String GET_TRENDS_END_DATE = "25-06-2013";
+
+    @Mock
+    private IndicatorService indicatorService;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private TrendController trendController = new TrendController();
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(trendController).build();
+    }
+
+    @Test
+    public void testGetTrends() throws Exception {
+        mockMvc.perform(get("/api/trend")
+                .param("startDate", GET_TRENDS_START_DATE)
+                .param("endDate", GET_TRENDS_END_DATE)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(userService, times(1)).getCurrentlyLoggedUser();
+        verify(indicatorService, times(1)).getIndicatorsWithTrendsUnderUser((UserEntity) anyObject(),
+                (Date) anyObject(), (Date) anyObject());
+    }
+
+}
