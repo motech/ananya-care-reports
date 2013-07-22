dba7af88f322341d0797d7438ea53068afe41d97
Pawel Reise <preise@soldevelo.com>
2013-07-22 14:16:17 +0200

    CARE #26: Extended test for Complex Condition Controller

--- src/test/java/org/motechproject/carereporting/web/controller/ComplexConditionControllerTest.java
+++ src/test/java/org/motechproject/carereporting/web/controller/ComplexConditionControllerTest.java
@@ -1,30 +1,24 @@
 package org.motechproject.carereporting.web.controller;
 
-import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.motechproject.carereporting.domain.ComplexConditionEntity;
-import org.motechproject.carereporting.domain.dto.DashboardPositionDto;
+import org.motechproject.carereporting.domain.dto.ComplexConditionDto;
 import org.motechproject.carereporting.service.ComplexConditionService;
-import org.motechproject.carereporting.service.DashboardService;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyObject;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -37,6 +31,16 @@ public class ComplexConditionControllerTest {
     private static final String TEST_COMPLEX_CONDITION_ID = "1";
 
     private static final String TEST_COMPLEX_CONDITION_NAME = "test_complex_condition";
+
+    private static final String CREATE_COMPLEX_CONDITION_JSON =
+            "{\"conditions\":[{\"comparisonSymbol\":{\"id\":3,\"name\":\"<\"},\"" +
+            "computedField\":{\"id\":407,\"name\":\"ifa_tablets\",\"type\":\"Number\"},\"comparisonValue\":90}]" +
+            ",\"name\":\"test cc\"}";
+
+    private static final String UPDATE_COMPLEX_CONDITION_JSON =
+            "{\"conditions\":[{\"comparisonSymbol\":{\"id\":3,\"name\":\"<\"},\"" +
+                    "computedField\":{\"id\":407,\"name\":\"ifa_tablets\",\"type\":\"Number\"},\"comparisonValue\":90}]" +
+                    ",\"name\":\"updated test cc\"}";
 
     @Mock
     private ComplexConditionService complexConditionService;
@@ -91,7 +95,37 @@ public class ComplexConditionControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("name").value(TEST_COMPLEX_CONDITION_NAME));
 
+
         verify(complexConditionService, times(1)).getComplexConditionById(anyInt());
+    }
+
+    @Test
+    public void testCreateNewComplexCondition() throws Exception {
+        mockMvc.perform(post("/api/complexcondition/")
+                .content(CREATE_COMPLEX_CONDITION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(complexConditionService, times(1)).createNewComplexCondition((ComplexConditionDto) anyObject());
+    }
+
+    @Test
+    public void testUpdateComplexCondition() throws Exception {
+        mockMvc.perform(put("/api/complexcondition/" + TEST_COMPLEX_CONDITION_ID)
+                .content(UPDATE_COMPLEX_CONDITION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(complexConditionService, times(1)).updateComplexCondition((ComplexConditionDto) anyObject());
+    }
+
+    @Test
+    public void testDeleteComplexCondition() throws Exception {
+        mockMvc.perform(delete("/api/complexcondition/" + TEST_COMPLEX_CONDITION_ID)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(complexConditionService, times(1)).deleteComplexCondition((ComplexConditionEntity) anyObject());
     }
 
 }
