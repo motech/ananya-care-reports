353225f9731ea02c63cd419dbd358acce9ddc13e
Adrian Boguszewski <aboguszewski@soldevelo.com>
2013-08-13 10:04:41 +0200

    CARE#37: Fixed tests errors

--- src/test/java/org/motechproject/carereporting/utils/date/DateResolverTest.java
+++ src/test/java/org/motechproject/carereporting/utils/date/DateResolverTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.motechproject.carereporting.domain.FrequencyEntity;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
@@ -16,32 +17,37 @@ public class DateResolverTest {
     private final Date END_DATE;
     private final Date START_DATE;
 
-    private final String START_DATE_FOR_DAY = "Tue Aug 13 00:00:00 CEST 2013";
-    private final String END_DATE_FOR_DAY = "Wed Aug 14 00:00:00 CEST 2013";
-    private final String START_DATE_FOR_WEEK = "Mon Aug 05 00:00:00 CEST 2013";
-    private final String END_DATE_FOR_WEEK = "Mon Aug 12 00:00:00 CEST 2013";
-    private final String START_DATE_FOR_MONTH = "Mon Jul 01 00:00:00 CEST 2013";
-    private final String END_DATE_FOR_MONTH = "Thu Aug 01 00:00:00 CEST 2013";
-    private final String START_DATE_FOR_QUARTER = "Mon Apr 01 00:00:00 CEST 2013";
-    private final String END_DATE_FOR_QUARTER = "Mon Jul 01 00:00:00 CEST 2013";
-    private final String START_DATE_FOR_YEAR = "Sun Jan 01 00:00:00 CET 2012";
-    private final String END_DATE_FOR_YEAR = "Tue Jan 01 00:00:00 CET 2013";
+    private final String START_DATE_FOR_DAY = "Tue Aug 13 00:00:00 2013";
+    private final String END_DATE_FOR_DAY = "Wed Aug 14 00:00:00 2013";
+    private final String START_DATE_FOR_WEEK = "Mon Aug 05 00:00:00 2013";
+    private final String END_DATE_FOR_WEEK = "Mon Aug 12 00:00:00 2013";
+    private final String START_DATE_FOR_MONTH = "Mon Jul 01 00:00:00 2013";
+    private final String END_DATE_FOR_MONTH = "Thu Aug 01 00:00:00 2013";
+    private final String START_DATE_FOR_QUARTER = "Mon Apr 01 00:00:00 2013";
+    private final String END_DATE_FOR_QUARTER = "Mon Jul 01 00:00:00 2013";
+    private final String START_DATE_FOR_YEAR = "Sun Jan 01 00:00:00 2012";
+    private final String END_DATE_FOR_YEAR = "Tue Jan 01 00:00:00 2013";
 
-    private final String START_DATE_FOR_DAY_2 = "Mon Mar 12 00:00:00 CET 2012";
-    private final String END_DATE_FOR_DAY_2 = "Thu Aug 15 00:00:00 CEST 2013";
-    private final String START_DATE_FOR_WEEK_2 = "Mon Mar 12 00:00:00 CET 2012";
-    private final String END_DATE_FOR_WEEK_2 = "Mon Aug 19 00:00:00 CEST 2013";
-    private final String START_DATE_FOR_MONTH_2 = "Thu Mar 01 00:00:00 CET 2012";
-    private final String END_DATE_FOR_MONTH_2 = "Sun Sep 01 00:00:00 CEST 2013";
-    private final String START_DATE_FOR_QUARTER_2 = "Sun Jan 01 00:00:00 CET 2012";
-    private final String END_DATE_FOR_QUARTER_2 = "Tue Oct 01 00:00:00 CEST 2013";
-    private final String START_DATE_FOR_YEAR_2 = "Sun Jan 01 00:00:00 CET 2012";
-    private final String END_DATE_FOR_YEAR_2 = "Wed Jan 01 00:00:00 CET 2014";
+    private final String START_DATE_FOR_DAY_2 = "Mon Mar 12 00:00:00 2012";
+    private final String END_DATE_FOR_DAY_2 = "Thu Aug 15 00:00:00 2013";
+    private final String START_DATE_FOR_WEEK_2 = "Mon Mar 12 00:00:00 2012";
+    private final String END_DATE_FOR_WEEK_2 = "Mon Aug 19 00:00:00 2013";
+    private final String START_DATE_FOR_MONTH_2 = "Thu Mar 01 00:00:00 2012";
+    private final String END_DATE_FOR_MONTH_2 = "Sun Sep 01 00:00:00 2013";
+    private final String START_DATE_FOR_QUARTER_2 = "Sun Jan 01 00:00:00 2012";
+    private final String END_DATE_FOR_QUARTER_2 = "Tue Oct 01 00:00:00 2013";
+    private final String START_DATE_FOR_YEAR_2 = "Sun Jan 01 00:00:00 2012";
+    private final String END_DATE_FOR_YEAR_2 = "Wed Jan 01 00:00:00 2014";
 
     public DateResolverTest() throws ParseException {
         START_DATE = DateUtils.parseDate("12.03.2012;13:12", new String[]{"dd.MM.yyyy;HH:mm"});
         END_DATE = DateUtils.parseDate("14.08.2013;14:41", new String[]{"dd.MM.yyyy;HH:mm"});
         frequencyEntity = new FrequencyEntity();
+    }
+
+    private String getStringFromDate(Date date) {
+        SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy");
+        return format.format(date);
     }
 
     @Test
@@ -50,8 +56,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, END_DATE);
 
-        assertEquals(START_DATE_FOR_DAY, dates[0].toString());
-        assertEquals(END_DATE_FOR_DAY, dates[1].toString());
+        assertEquals(START_DATE_FOR_DAY, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_DAY, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -60,8 +66,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, END_DATE);
 
-        assertEquals(START_DATE_FOR_WEEK, dates[0].toString());
-        assertEquals(END_DATE_FOR_WEEK, dates[1].toString());
+        assertEquals(START_DATE_FOR_WEEK, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_WEEK, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -70,8 +76,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, END_DATE);
 
-        assertEquals(START_DATE_FOR_MONTH, dates[0].toString());
-        assertEquals(END_DATE_FOR_MONTH, dates[1].toString());
+        assertEquals(START_DATE_FOR_MONTH, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_MONTH, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -80,8 +86,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, END_DATE);
 
-        assertEquals(START_DATE_FOR_QUARTER, dates[0].toString());
-        assertEquals(END_DATE_FOR_QUARTER, dates[1].toString());
+        assertEquals(START_DATE_FOR_QUARTER, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_QUARTER, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -90,8 +96,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, END_DATE);
 
-        assertEquals(START_DATE_FOR_YEAR, dates[0].toString());
-        assertEquals(END_DATE_FOR_YEAR, dates[1].toString());
+        assertEquals(START_DATE_FOR_YEAR, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_YEAR, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -100,8 +106,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, START_DATE, END_DATE);
 
-        assertEquals(START_DATE_FOR_DAY_2, dates[0].toString());
-        assertEquals(END_DATE_FOR_DAY_2, dates[1].toString());
+        assertEquals(START_DATE_FOR_DAY_2, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_DAY_2, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -110,8 +116,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, START_DATE, END_DATE);
 
-        assertEquals(START_DATE_FOR_WEEK_2, dates[0].toString());
-        assertEquals(END_DATE_FOR_WEEK_2, dates[1].toString());
+        assertEquals(START_DATE_FOR_WEEK_2, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_WEEK_2, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -120,8 +126,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, START_DATE, END_DATE);
 
-        assertEquals(START_DATE_FOR_MONTH_2, dates[0].toString());
-        assertEquals(END_DATE_FOR_MONTH_2, dates[1].toString());
+        assertEquals(START_DATE_FOR_MONTH_2, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_MONTH_2, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -130,8 +136,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, START_DATE, END_DATE);
 
-        assertEquals(START_DATE_FOR_QUARTER_2, dates[0].toString());
-        assertEquals(END_DATE_FOR_QUARTER_2, dates[1].toString());
+        assertEquals(START_DATE_FOR_QUARTER_2, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_QUARTER_2, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -140,8 +146,8 @@ public class DateResolverTest {
 
         Date[] dates = DateResolver.resolveDates(frequencyEntity, START_DATE, END_DATE);
 
-        assertEquals(START_DATE_FOR_YEAR_2, dates[0].toString());
-        assertEquals(END_DATE_FOR_YEAR_2, dates[1].toString());
+        assertEquals(START_DATE_FOR_YEAR_2, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_YEAR_2, getStringFromDate(dates[1]));
     }
 
     @Test
@@ -151,8 +157,8 @@ public class DateResolverTest {
         Date date = DateUtils.addDays(END_DATE, -1);
         Date[] dates = DateResolver.resolveDates(frequencyEntity, date, date);
 
-        assertEquals(START_DATE_FOR_DAY, dates[0].toString());
-        assertEquals(END_DATE_FOR_DAY, dates[1].toString());
+        assertEquals(START_DATE_FOR_DAY, getStringFromDate(dates[0]));
+        assertEquals(END_DATE_FOR_DAY, getStringFromDate(dates[1]));
     }
 
 }
