4cfe3cc76af9dc3c2dc26e32e836dd491434cfb8
Pawel Reise <preise@soldevelo.com>
2013-09-10 14:32:11 +0200

    CARE #10: Create trigger on insert new area to reporting db

--- src/main/resources/db/insert_area_data.sql
+++ src/main/resources/db/insert_area_data.sql
@@ -32,3 +32,41 @@ END'
 LANGUAGE 'plpgsql';
 
 select fetchAreasFromReportingDB();
+
+CREATE OR REPLACE FUNCTION dashboard_app.populateAreaFunction() RETURNS trigger AS
+E'BEGIN
+    IF (select distinct area_id from dashboard_app.area where name = NEW.state
+    and level_id = (select level_id from dashboard_app.level where name = \'state\')) IS NULL THEN
+        INSERT INTO DASHBOARD_APP.AREA (name, level_id, parent_area_id, creation_date, modification_date)
+        VALUES (NEW.state, (select level_id from dashboard_app.level where name = \'state\'), null, now(), now());
+    END IF;
+    IF (select distinct area_id from dashboard_app.area where name = NEW.district
+    and level_id = (select level_id from dashboard_app.level where name = \'district\')) IS NULL THEN
+        INSERT INTO DASHBOARD_APP.AREA (name, level_id, parent_area_id, creation_date, modification_date)
+        VALUES (NEW.district, (select level_id from dashboard_app.level where name = \'district\'),
+            (select area_id from dashboard_app.area where
+            name = (select distinct state from report.location_dimension
+                where district = NEW.district)
+            and level_id = (select level_id from dashboard_app.level where name = \'state\')),
+            now(), now());
+    END IF;
+    IF (select distinct area_id from dashboard_app.area where name = NEW.block
+    and level_id = (select level_id from dashboard_app.level where name = \'block\')) IS NULL THEN
+        INSERT INTO DASHBOARD_APP.AREA (name, level_id, parent_area_id, creation_date, modification_date)
+        VALUES (NEW.block, (select level_id from dashboard_app.level where name = \'block\'),
+            (select area_id from dashboard_app.area where
+            name = (select distinct district from report.location_dimension
+                where block = NEW.block)
+            and level_id = (select level_id from dashboard_app.level where name = \'district\')),
+            now(), now());
+    END IF;
+    RETURN NEW;
+END'
+LANGUAGE 'plpgsql';
+
+drop trigger if exists populateAreaTrigger on report.location_dimension;
+
+CREATE TRIGGER populateAreaTrigger AFTER INSERT
+    ON report.location_dimension
+    FOR EACH ROW
+    EXECUTE PROCEDURE dashboard_app.populateAreaFunction();
