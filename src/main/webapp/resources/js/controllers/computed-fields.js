39259715d7473a3a03295420c3cc6e3d99d01339
Krzysztof Kaczmarczyk <kkaczmarczyk@soldevelo.com>
2013-09-13 14:05:24 +0200

    Computed field list fix

--- src/main/webapp/resources/js/controllers/computed-fields.js
+++ src/main/webapp/resources/js/controllers/computed-fields.js
@@ -98,8 +98,8 @@ care.controller('computedFieldsController', function($scope, $http, $routeParams
     };
 
     $scope.removeField = function(key) {
-        $scope.fields.push($scope.selectedFields[key].field);
-        $scope.fields.sortByField('name');
+        //$scope.fields.push($scope.selectedFields[key].field);
+        //$scope.fields.sortByField('name');
         $scope.selectedField = $scope.fields[0].id;
 
         $scope.selectedFields.splice(key, 1);
