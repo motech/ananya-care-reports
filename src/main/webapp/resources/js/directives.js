0c3388ac177c0ed4cc038bc0f19442b5817fa297
Adrian Boguszewski <aboguszewski@soldevelo.com>
2013-09-27 15:24:48 +0200

    CARE #100: Add prompts with clarifications for users

--- src/main/webapp/resources/js/directives.js
+++ src/main/webapp/resources/js/directives.js
@@ -5,57 +5,21 @@
 
     var widgetModule = angular.module('care');
 
-    widgetModule.directive('userPopover', function () {
+    widgetModule.directive('tip', function ($timeout) {
        return {
-           restrict: 'A',
+           restrict: 'E',
            link: function (scope, element, attrs) {
-                angular.element(element).popover({
-                    placement: 'left',
-                    trigger: 'hover',
-                    html: true,
-                    title: scope.msg('users.list.popoverTitle') + ":",
-                    content: function () {
-                        var html = angular.element('<ul />');
-                        for (var i in scope.user.roles) {
-                            if (scope.user.roles.hasOwnProperty(i)) {
-                                var li = angular.element('<li />');
-                                li.text(scope.user.roles[i].name);
-                                html.append(li);
-                            }
-                        }
-
-                        return html;
-                    }
+           $timeout(function() {
+                    element.append(angular.element('<span class="add-on btn"><i class="icon-question-sign"></i></span>'))
+                    .popover({
+                        placement: 'right',
+                        trigger: 'click',
+                        content:  scope.msg(attrs.msg)
+                    });
                 });
            }
        };
     });
-
-    widgetModule.directive('rolePopover', function () {
-           return {
-               restrict: 'A',
-               link: function (scope, element, attrs) {
-                    angular.element(element).popover({
-                        placement: 'left',
-                        trigger: 'hover',
-                        html: true,
-                        title: scope.msg('roles.list.popoverTitle') + ":",
-                        content: function () {
-                            var html = angular.element('<ul />');
-                            for (var i in scope.role.permissions) {
-                                if (scope.role.permissions.hasOwnProperty(i)) {
-                                    var li = angular.element('<li />');
-                                    li.text(scope.role.permissions[i].displayName);
-                                    html.append(li);
-                                }
-                            }
-
-                            return html;
-                        }
-                    });
-               }
-           };
-        });
 
         widgetModule.directive('indicatorsTrend', function () {
                return {
